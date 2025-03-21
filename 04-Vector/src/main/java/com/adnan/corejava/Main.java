3a17f4c1c2b7890fb6014579b4a45b037329c845
Mohd Adnan Khan <mohdadnan02072002@gmail.com>
2025-03-21 15:19:23 +0530

    ArrayList v/s Vector

--- 04-Vector/src/main/java/com/adnan/corejava/Main.java
+++ 04-Vector/src/main/java/com/adnan/corejava/Main.java
@@ -1,5 +1,6 @@
 package com.adnan.corejava;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Vector;
 
@@ -45,7 +46,67 @@ public class Main {
 		vector1.clear();
 
 		System.out.println(vector1);
-		
+
+		// vector is synchronized
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+		Thread t1 = new Thread(() -> {
+			for (int i = 0; i < 1000; i++) {
+				list.add(i);
+			}
+		});
+
+		Thread t2 = new Thread(() -> {
+			for (int i = 0; i < 1000; i++) {
+				list.add(i);
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Size of list : " + list.size()); // 1000+1000=2000 list.size() should be 2000
+		// but it will not return 2000 as it is not thread save or in other words we can
+		// say it is not synchronous/synchronized
+
+		// now using vector
+
+		Vector<Integer> vector = new Vector<Integer>();
+
+		Thread t3 = new Thread(() -> {
+			for (int i = 0; i < 1000; i++) {
+				vector.add(i);
+			}
+		});
+
+		Thread t4 = new Thread(() -> {
+			for (int i = 0; i < 1000; i++) {
+				vector.add(i);
+			}
+		});
+
+		t3.start();
+		t4.start();
+
+		try {
+			t3.join();
+			t4.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Size of list : " + vector.size()); // 1000+1000=2000 vector.size() should be 2000
+		// but it return 2000 as it is thread save or in other words we can say it is
+		// synchronous/synchronized
+
 	}
 
 }
